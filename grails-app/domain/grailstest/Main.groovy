1c2374a748406e3c1e5ff82082bb37983cb84819
senchyna <dazzasenchy@gmail.com>
2018-07-19 12:30:55 +0100

    Trying to actually make it a usable app

---
+++ grails-app/domain/grailstest/Main.groovy
@@ -0,0 +1,7 @@
+package grailstest
+
+class Main {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/grailstest/MainTests.groovy
@@ -0,0 +1,17 @@
+package grailstest
+
+import grails.test.*
+
+class MainTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}

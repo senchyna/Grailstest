7b495708c36f617f63b868160d5c1d48d600f295
senchyna <dazzasenchy@gmail.com>
2018-07-19 12:33:55 +0100

    adding some code from a website i found

--- grails-app/domain/grailstest/Main.groovy
+++ grails-app/domain/grailstest/Main.groovy
@@ -2,6 +2,13 @@ package grailstest
 
 class Main {
 
+	String name 
+    String make
+    String model
+	
     static constraints = {
+	    name maxSize: 255
+        make inList: ['Ford', 'Chevrolet', 'Nissan']
+        model nullable: true
     }
 }
